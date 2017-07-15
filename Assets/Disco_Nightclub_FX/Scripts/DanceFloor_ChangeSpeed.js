d2be250dc9bc3a75e342ff959b56e5d70de89c36
AnHyoungBin <nks00404@gmail.com>
2017-07-15 11:41:24 +0900

    add VRTK & Drum sound

---
+++ Assets/Disco_Nightclub_FX/Scripts/DanceFloor_ChangeSpeed.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+// var danceFloorAnimSpeedTemp : int;
+// static var danceFloorAnimSpeed : int;
+
+//public static class Settings {
+public var animSpeedValue : float;
+public static var animSpeed : float;
+
+DanceFloor_ChangeSpeed.animSpeed = animSpeedValue;
+
+
+function Start () {
+
+
+
+}
+
+function Update () {
+
+			
+}
